1720b8ad9fbb61c4fcee2ea0859c1bacd2811c51
Ro <rodnymm21@gmail.com>
2023-11-21 19:44:56 -0600

    my_third_commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 numeros = [1, 2, 3, 4, 5]
+
+def multiplicar_lista(lista):
+    resultado = 1
+    for elemento in lista:
+        resultado *= elemento
+    return resultado
+
+print(multiplicar_lista(numeros))
